74b9a3e99ca05999153cea41aba8b1fc7f1d46fe
Tounga-Saidou <23saiodu@gmail.com>
2024-06-20 09:53:17 +0200

    Catelog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CatelogComponent } from './catelog/catelog.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },  // Default route to HomeComponent
+  { path: 'home', component: LandingPageComponent},
+  { path: 'catelog', component: CatelogComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '/home' }  // Wildcard route for handling 404 not found
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
